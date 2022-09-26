dcced679d397b7f7747115d1a7e8741de2779dbb
Set Programming <setprogramming@gmail.com>
2022-09-26 11:41:36 +0300

    working on Property Details

--- components/Property.js
+++ components/Property.js
@@ -2,14 +2,14 @@ import Link from "next/link"
 import Image from "next/image"
 import { Box, Flex, Text, Avatar } from "@chakra-ui/react"
 import {FaBed, FaBath} from "react-icons/fa"
-import {BsGridFill} from "react-icons/bs"
+import {BsGridFill} from "react-icons/bs"     
 import {GoVerified} from "react-icons/go"
 import millify from "millify"
 
 import defaultImage from "../assets/images/house.jpg"
 
-const Property = ({property: {coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalId}}) => (
-    <Link href={`/property/${externalId}`} passHref>
+const Property = ({property: {coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID}}) => (
+    <Link href={`/property/${externalID}`} passHref>
         <Flex flexWrap="wrap" w="420px" p="5" paddingTop="0" justifyContent="flex-start" cursor="pointer">
             <Box>
                 <Image 
--- components/SearchFilters.js
+++ components/SearchFilters.js
@@ -1,23 +1,42 @@
 import {useEffect, useState} from "react"
 import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from "@chakra-ui/react"
-import {useRouter} from "next/router"
+import router, {useRouter} from "next/router"
 import {MdCancel} from "react-icons/md"
 import Image from "next/image"
 import { filterData, getFilterValues } from "../utils/filterData"
+import { baseUrl, fetchApi } from '../utils/fetchApi'
 
-const SearchFilters = () => {
-    const [filters, setFilters] = useState(filterData)
+export default function SearchFilters() {
+    const [filters] = useState(filterData)
+    const router = useRouter()
 
-    const searchProperties = () => {
-        
+    const searchProperties = (filterValues) => {
+        const path = router.pathname
+        const {query} = router
+        const values = getFilterValues(filterValues)        
+
+        values.forEach((item) => {
+            query[item.name] = item.value
+        })
+
+        router.push({pathname: path, query})
     }
 
-    return (
+    return (             
         <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
             {filters.map((filter) => (
                 <Box key={filter.queryName}>
-                    <Select onChange={(e) => searchProperties()}>
-
+                    <Select 
+                        placeholder={filter.placeholder}
+                        w="fit-content"
+                        p="2"
+                        onChange={(e) => searchProperties({[filter.queryName]: e.target.value})}
+                    >
+                        {filter?.items?.map((item) => (
+                            <option value={item.value} key={item.value}>
+                                {item.name}
+                            </option>
+                        ))}
                     </Select>
                 </Box>
             ))}
@@ -25,4 +44,3 @@ const SearchFilters = () => {
     )
 }
 
-export default SearchFilters
